f3bd92715366e0b5791aa6fc346c882021144db7
Frederick Bläsing <natrium@frederick.blaes.ing>
2026-03-26 03:39:07 +0100

    add lab-help command

--- hosts/lab-manager/configuration.nix
+++ hosts/lab-manager/configuration.nix
@@ -8,8 +8,42 @@
 }: let
   inherit (pkgs.stdenv.hostPlatform) system;
   diskoPkgs = inputs.disko.packages.${system};
+  clientConfig = self.legacyPackages.${system}.nixosConfigurations.lab-client-1.config;
 
-  targetConfig = self.legacyPackages.${system}.nixosConfigurations.lab-client-1.config;
+  labHelp = ''
+    To set up a wireless connection, run:
+    > nmtui
+
+    To copy and enter the client configuration, run:
+    > cp --recursive --no-preserve all /etc/nixos swesbus-lab
+    > cd swesbus-lab
+
+    To read the documentation, run:
+    > less README.md
+
+    To edit the client configuration, run:
+    > nano hosts/lab-client/configuration.nix
+
+    To list available disks, run:
+    > sudo lsblk -l
+
+    To install the client with index 1 to disk "sda", run:
+    > disko-install --flake .#lab-client-1 --disk main /dev/sda
+    This will *irrevocably* erase all data on that disk!
+
+    To reboot the system, run:
+    > sudo reboot
+
+    To show this message again, run:
+    > lab-help
+  '';
+
+  labHelpBin =
+    pkgs.writeShellScriptBin "lab-help"
+    # bash
+    ''
+      cat ${pkgs.writeText "lab-help-text" labHelp}
+    '';
 in {
   imports = [(modulesPath + "/installer/cd-dvd/iso-image.nix")];
 
@@ -35,31 +69,7 @@ in {
   services.getty = {
     autologinUser = "admin";
     greetingLine = "Welcome to the lab manager - ${config.system.nixos.distroName} ${config.system.nixos.label} (\m) - \l";
-
-    helpLine = ''
-      To set up a wireless connection, run:
-      > nmtui
-
-      To copy and enter the client configuration, run:
-      > cp --recursive --no-preserve all /etc/nixos swesbus-lab
-      > cd swesbus-lab
-
-      To read the documentation, run:
-      > less README.md
-
-      To edit the client configuration, run:
-      > nano hosts/lab-client/configuration.nix
-
-      To list available disks, run:
-      > sudo lsblk -l
-
-      To install the client with index 1 to disk "sda", run:
-      > disko-install --flake .#lab-client-1 --disk main /dev/sda
-      This will *irrevocably* erase all data on that disk!
-
-      To reboot the system, run:
-      > sudo reboot
-    '';
+    helpLine = labHelp;
   };
 
   # Networking
@@ -74,10 +84,11 @@ in {
   # Packages
   environment.systemPackages = [
     diskoPkgs.disko # Disk partitioning
+    labHelpBin
     pkgs.cage
     pkgs.freerdp
     pkgs.nano
-    targetConfig.system.build.toplevel # Bundle target configuration
     pkgs.vim
+    clientConfig.system.build.toplevel # Bundle target configuration
   ];
 }
