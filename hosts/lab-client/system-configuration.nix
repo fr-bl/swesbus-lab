43ec56e9624bf824331a880f84b63d3fc042eea3
Frederick Bläsing <natrium@frederick.blaes.ing>
2026-04-02 13:41:53 +0200

    remove passwordless login

--- hosts/lab-client/system-configuration.nix
+++ hosts/lab-client/system-configuration.nix
@@ -18,8 +18,6 @@ in {
     };
 
     # Users
-    users.groups.nopasswdlogin = {};
-    
     users.users.admin = {
       description = "Admin";
       isNormalUser = true;
@@ -29,7 +27,6 @@ in {
     users.users.remote = {
       description = "Remote Session";
       isNormalUser = true;
-      extraGroups = ["nopasswdlogin"];
       hashedPassword = "";
     };
 
@@ -41,10 +38,6 @@ in {
       defaultSession = "lab-remote-session";
       autoLogin.user = "remote";
     };
-
-    security.pam.services.gdm-password.text = ''
-      auth sufficient pam_succeed_if.so user ingroup nopasswdlogin
-    '';
 
     systemd.services.display-manager = {
       wants = ["network-online.target"];
